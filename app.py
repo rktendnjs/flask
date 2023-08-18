9ad1042f5408d2605e1abf24e2c379487aed24be
rktendnjs <133492034+rktendnjs@users.noreply.github.com>
2023-08-19 03:22:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -357,7 +357,7 @@ def perform_address_search(search_data):
     return []
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, timeout=1200)  
+    app.run(host="0.0.0.0", port=8000)  
 
 
 
