823ba15d474a6974dd55be64c386edc263dadd62
rktendnjs <133492034+rktendnjs@users.noreply.github.com>
2023-08-19 12:23:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import re
 import pandas as pd
 import requests
 from flask import Flask, jsonify, request
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
 
 app = Flask(__name__)
 
@@ -333,6 +331,7 @@ def search():
 
 
 
+
 def perform_address_search(search_data):
     api_key = 'devU01TX0FVVEgyMDIzMDcyODE1MzkzNzExMzk3MzA='
     base_url = 'http://www.juso.go.kr/addrlink/addrLinkApi.do'
@@ -344,36 +343,19 @@ def perform_address_search(search_data):
         'resultType': 'json',
         'keyword': search_data,
     }
-    
-    MAX_RETRY = 5000
-    RETRY_WAIT_SECONDS = 10
 
-    for retry_count in range(MAX_RETRY):
-        try:
-            # Configure retries and timeout
-            retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[ 500, 502, 503, 504 ])
-            session = requests.Session()
-            adapter = HTTPAdapter(max_retries=retries)
-            session.mount('http://', adapter)
-            session.mount('https://', adapter)
+    response = requests.get(base_url, params=payload)
 
-            response = session.get(base_url, params=payload, timeout=(10, 1200))  # 커넥션 타임아웃과 리드 타임아웃 설정
-
-            if response.status_code == 200:
-                search_result = response.json()
-                if 'results' in search_result and 'juso' in search_result['results']:
-                    result_data = search_result['results']['juso']
-                    if result_data:
-                        # Extract and return the road addresses from the API response
-                        return [result.get('roadAddr', '') for result in result_data]
-
-            return []
-
-        except Exception as e:
-            print(f"Error occurred, retrying in {RETRY_WAIT_SECONDS} seconds...")
-            time.sleep(RETRY_WAIT_SECONDS)
+    if response.status_code == 200:
+        search_result = response.json()
+        if 'results' in search_result and 'juso' in search_result['results']:
+            result_data = search_result['results']['juso']
+            if result_data:
+                # Extract and return the road addresses from the API response
+                return [result.get('roadAddr', '') for result in result_data]
 
     return []
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)  
 
